6a27cecb63d38834f0b6781d8f3c7791ac7c6bb7
apollothecodemeleon <kaden.hines@bridges.edu>
2024-10-11 17:57:57 -0700

    Made a door along with a raycast for the player that opens that door,  but it doesn't work right now
    it's currently returning the error "cannot call method 'play' on a null value" and i am troubleshooting that right now

---
+++ projectshift/Scripts/door.gd
@@ -0,0 +1,16 @@
+extends StaticBody3D
+
+var toggle = false
+var interactable = true
+@export var animation_player: AnimationPlayer
+
+func interact():
+	if interactable == true:
+		interactable = false  
+		toggle = !toggle
+		if toggle == false:
+			animation_player.play("doorClose")
+		if toggle == true:
+			animation_player.play("doorOpen")
+		await get_tree().create_timer(1.0, false).timeout
+		interactable = true
