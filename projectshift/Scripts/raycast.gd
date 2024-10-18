07bfb60b1cc8037f93eeeb2f4d45be755539e37e
Kaden Hines <kaden.hines@bridges.edu>
2024-10-18 12:37:30 -0700

    Added comments to interact raycast script

--- projectshift/Scripts/raycast.gd
+++ projectshift/Scripts/raycast.gd
@@ -1,8 +1,9 @@
 extends RayCast3D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since the previous frame
+#this script basically makes it so that:
 func _process(_delta):
-	if is_colliding():
-		var hitObj = get_collider()
-		if hitObj != null and hitObj.has_method("interact") and Input.is_action_just_pressed("interact"):
-			hitObj.interact()
+	if is_colliding():#when your raycast hits something
+		var hitObj = get_collider()#check what it is
+		if hitObj != null and hitObj.has_method("interact") and Input.is_action_just_pressed("interact"):#and if you can interact with it, and you press the interact key,
+			hitObj.interact()#trigger the interact method in the thing you hit
