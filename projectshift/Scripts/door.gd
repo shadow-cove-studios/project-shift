d7e88bbf1686dad245148267432b4cd38ab4eb21
Kaden Hines <kaden.hines@bridges.edu>
2024-11-08 13:14:59 -0800

    added comments to the door script that help explain it's complex logic

--- projectshift/Scripts/door.gd
+++ projectshift/Scripts/door.gd
@@ -6,12 +6,13 @@ var interactable = true
 
 
 func interact():
-	if interactable == true:
-		interactable = false  
-		toggle = !toggle
-		if toggle == false:
+	if interactable == true: #if interactable is true
+		interactable = false  #interactable is set to false to avoid multiple interaction
+		toggle = !toggle #it then makes the toggle the opposite of what it was before
+		if toggle == false: #if toggle is false then it plays the close door animation
 			animation_player.play("doorClose")
-		if toggle == true:
+		if toggle == true:# and if it is true it  plays the open door animation
 			animation_player.play("doorOpen")
+		#after the animation starts it waits 1 sec before it can be toggled again
 		await get_tree().create_timer(1.0, false).timeout
 		interactable = true
