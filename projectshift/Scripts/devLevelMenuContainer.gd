11239596e12a716c1d16e3e9368a70c30a90dfe9
Voxel-Cat <117233448+Voxel-Cat@users.noreply.github.com>
2025-05-30 12:58:51 -0700

    Added level Prototyping scene to levels
    changes:
    - added new button to dev level select screen
    - linked level prototyping to new button (i also added the code to the existing container script)

--- projectshift/Scripts/devLevelMenuContainer.gd
+++ projectshift/Scripts/devLevelMenuContainer.gd
@@ -1,8 +1,10 @@
 extends VBoxContainer
 
 const MainTestLevel = preload("res://Scenes/node_3d.tscn")
-
+const levelprototyping = preload("res://Scenes/levelPrototyping.tscn")
 
 
 func _on_main_test_level_button_pressed():
 		get_tree().change_scene_to_packed(MainTestLevel)
+func _on_lvlproto_pressed():
+	get_tree().change_scene_to_packed(levelprototyping)
