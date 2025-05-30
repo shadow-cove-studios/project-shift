586c57ca2127b010d351bd4eb6aa76a06b9dd20c
Voxel-Cat <117233448+Voxel-Cat@users.noreply.github.com>
2025-05-30 14:58:10 -0700

    wall running
    ...

--- projectshift/Scripts/Player.gd
+++ projectshift/Scripts/Player.gd
@@ -60,9 +60,14 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY# please don't remove it
 	
 	#this makes the player walljump
-	if Input.is_action_just_pressed("ui_accept") and is_on_wall() and walljumpcount == 0:
+	if Input.is_action_just_pressed("ui_accept") and is_on_wall() and not is_on_floor() and walljumpcount < 2:
 		velocity.y = JUMP_VELOCITY# please don't remove it
-		walljumpcount = 1
+		walljumpcount = walljumpcount
+	if Input.is_action_pressed("sprint")	 and is_on_wall() and not is_on_floor():
+		gravity = 1
+	if not Input.is_action_pressed("sprint")	 and not is_on_floor():
+		gravity = 11
+		
 	
 	if Input.is_action_just_pressed("shift"):
 		if shifted == false:
