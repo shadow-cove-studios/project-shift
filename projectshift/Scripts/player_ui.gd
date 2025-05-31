08283437319b4f3f92c496d1d26ccac7d3f66b92
apollothecodemeleon <kaden.hines@bridges.edu>
2025-05-31 16:36:24 -0700

    Revert "broke the camera"
    This reverts commit ad11544b67d5d06609a0d38fa3d0513789d28ad6.

--- projectshift/Scripts/player_ui.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-@onready var shiftIndicator =  $RichTextLabel
-func _process(delta):
-	if PlayerVariables.canshift == true:
-		shiftIndicator.text = "Can shift"
-	else:
-		shiftIndicator.text = "False"
